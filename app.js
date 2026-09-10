ed123461fddd4da5220766f761a6e96a5edbd856
cristinaraimondo <cristinaraimondo@hotmail.com>
2026-09-09 22:42:16 -0300

    otro arreglo los QR

--- app.js
+++ app.js
@@ -186,7 +186,7 @@ if (logoArchivo) {
 
   qrContainer.innerHTML = "";
 
-  const slug = `${Date.now()}-${Math.random().toString(36).slice(2, 10)}`;
+const slug = `${Date.now()}-${Math.random().toString(36).slice(2, 10)}`;
  const urlPublica = new URL(`q.html?slug=${slug}`, window.location.href).href;
 
  new QRCode(qrContainer, {
